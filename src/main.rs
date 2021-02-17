517b1b1422915b254e31a4f5f7113f17261e34d5
m.kiida <mitsuo1110@gmail.com>
2021-02-17 23:09:27 +0900

    01

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+// メイン関数
 fn main() {
-    println!("Hello, world!");
+    nlp00();
+    nlp01();
+}
+
+// 00. 文字列の逆順
+fn nlp00() {
+    let str1 = "stressed";
+    let str2 = str1.chars().rev().collect::<String>();
+    println!("{}", str2);
+}
+
+// 01. 「パタトクカシーー」
+fn nlp01() {
+    let str1 = "「パタトクカシーー」";
+    let str1len = str1.chars().count();
+    let mut number = 0;
+    for element in str1.chars() {
+        if number < str1len - 1 {
+            if number % 2 == 1 {
+                println!("{}", element);
+            }
+        }
+        number = number + 1;
+    }
 }
